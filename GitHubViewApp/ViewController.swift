d67813e3a191a456cfd2001562d140309e716bd3
hisatoshi.tabuchi <hisatoshi.tabuchi@arsaga.jp>
2022-12-05 12:01:23 +0900

    Initial Commit

---
+++ GitHubViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubViewApp
+//
+//  Created by Tabuchi Hisatoshi on 2022/12/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
